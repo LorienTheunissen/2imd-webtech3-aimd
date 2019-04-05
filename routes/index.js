fcbcb2d2dbbad173862228778b05afa4bc3dced0
Lorien <r0622279@student.thomasmore.be>
2019-04-05 11:39:34 +0200

    AIMD / added controller

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var messageController = require('../controllers/message');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
