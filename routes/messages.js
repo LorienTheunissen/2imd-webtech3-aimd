8cddf29d326576b63e5234d6360da2e0e5d24cfd
Lorien <r0622279@student.thomasmore.be>
2019-05-03 10:59:30 +0200

    AIMD / separated message routes

---
+++ routes/messages.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+var messageController = require('../controllers/message');
+
+// POST message 
+router.post('/api/v1/messages', messageController.postMessages);
+
+// GET messages
+router.get('/api/v1/messages', messageController.getMessages);
+
+// GET message
+router.get('/api/v1/messages/:id', messageController.getMessage);
+
+// DELETE message
+router.delete('/api/v1/messages/:id', messageController.deleteMessage);
+
+// PUT (update) message
+router.put('/api/v1/messages/:id', messageController.putMessage);
+
+module.exports = router;
