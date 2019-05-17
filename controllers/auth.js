856769c83971799bdc869c12f04c16acf34fff8a
Lorien <r0622279@student.thomasmore.be>
2019-05-17 10:38:09 +0200

    AIMD / Inloggen via web tokens

--- controllers/auth.js
+++ controllers/auth.js
@@ -33,12 +33,27 @@ const signup = async (req, res, next) => {
 
 const login = async(req, res, next) => {
     const user = await User.authenticate()(req.body.username, req.body.password).then(result => {
-        res.json({
+        console.log(result);
+        
+        if(!result.user){
+            return res.json({
+                "status": "failed",
+                "message": "Login failed"
+            });
+        }
+        
+        let token = jwt.sign({
+            uid: result.user._id,
+            username: result.user.username
+        }, "MyVerySecretWord");
+
+        return res.json({
             "status": "success",
             "data": {
-                "user": result
+                "token": token
             }
         });
+
     }).catch(error => {
         res.json({
             "status": "error",
