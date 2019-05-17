36635dde084e7a3454eb4ffc927a9d57d15be386
Lorien <r0622279@student.thomasmore.be>
2019-05-17 10:09:31 +0200

    AIMD / fixed problem during merge

--- routes/api/v1/messages.js
+++ routes/api/v1/messages.js
@@ -1,15 +1,6 @@
 const express = require('express');
 const router = express.Router();
-<<<<<<< HEAD
-const messageController = require("../../../controllers/message")
 
-
-router.get("/", messageController.getMessages);
-router.post("/", messageController.postMessages);
-router.get("/:id", messageController.getMessages);
-router.delete("/:id", messageController.deleteMessages);
-router.put("/:id", messageController.putMessages);
-=======
 const messagesController = require('./../../../controllers/api/v1/messages')
 
 /* api/v1/messages */
@@ -22,6 +13,5 @@ router.get('/:id', messagesController.getMessage);
 router.delete('/:id', messagesController.deleteMessage);
 
 router.put('/:id', messagesController.putMessage);
->>>>>>> 40f90d381ed40c3c64567ec800465f1506204dbe
 
 module.exports = router;
