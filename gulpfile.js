dbe035fde0ee1418356a53b8e74106fb9b8fa48a
snipercool <sniper.cool@hotmail.com>
2019-05-22 19:40:49 +0200

    gulp done featuring sizing to see how big file is and nodemon added

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,8 @@
 const{ src, dest, watch, series } = require('gulp');
 const sass = require('gulp-sass');
-/*function pizza() {
-    return src("./*.md")
-    .pipe(dest("./markdown"));   
-}*/
+const nodemon = require('gulp-nodemon');
+const size = require('gulp-size');
+const notify = require('gulp-notify');
 
 function sass2css(done) {
     src("./public/sass/app.scss")
@@ -11,12 +10,35 @@ function sass2css(done) {
             outputStyle:'compressed'
         }).on('error', sass.logError))
         .pipe(dest("./dist/css/"))
-        .pipe(browserSync.stream());
 
     done();
+}
+function startnodemon(done) {
+    var started = false;
+    return nodemon({
+        script: 'app.js'
+    }).on('start', function() {
+        if (!started) {
+            done();
+            started=true;
+        }
+    })
+    
+}
+function sizing(done) {
+    const kb = size();
+    src("./app.js")
+    .pipe(kb)
+    .pipe(dest('./'))
+    .pipe(notify ({
+        onLast:true,
+        message: () => `Total size = ${kb.prettySize}`
+        
+    }));
+    
 }
 
 watch('./public/sass/**/*.scss', sass2css);
 
 
-module.exports.default = series(sass2css);
+module.exports.default = series(startnodemon, sass2css, sizing);
--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -25,3 +25,33 @@ document.querySelector("#register_button").addEventListener("click", (e) => {
         }
     })
 });
+document.querySelector(".submit").addEventListener("click", (e) => {
+ 
+    let username = document.querySelector(".username").value;
+    let password = document.querySelector(".password").value;
+ 
+    fetch('/login', {
+        method: "post",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            "username": username,
+            "password": password
+        })
+    }).then(response => {
+        return response.json();
+    }).then(json => {
+        if (json.status === "success") {
+            let token = json.data.token;
+            localStorage.setItem("token", token);
+            window.location.href = "/";
+        }
+        else {
+            let feedback = document.querySelector(".feedback");
+            feedback.textContent = "Login failed."
+            feedback.classList.remove("hidden");
+        }
+ 
+    })
+});
